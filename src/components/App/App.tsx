a3a81993587d56bc2d8fc105189313810db6c65b
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-10-15 10:28:07 -0300

    add: pagina admin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "../index";
 import {
+  Admin,
   Competencias,
   Contacto,
   Federacion,
   Inicio,
   Noticias,
 } from "../../pages";
+import { Box } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/federacion" element={<Federacion />} />
-        <Route path="/competencias" element={<Competencias />} />
-        <Route path="/noticias" element={<Noticias />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
+      <Box maxW="1200px" mx="auto" px={4}>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/federacion" element={<Federacion />} />
+          <Route path="/competencias" element={<Competencias />} />
+          <Route path="/noticias" element={<Noticias />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Box>
     </div>
   );
 };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Federacion } from "./Federacion/Federacion";
 export { default as Competencias } from "./Competencias/Competencias";
 export { default as Noticias } from "./Noticias/Noticias";
 export { default as Contacto } from "./Contacto/Contacto";
+export { default as Admin } from "./Admin/Admin";

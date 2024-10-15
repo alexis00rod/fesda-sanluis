093b822acb9c90abf01c102710ace861fc3151ad
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-10-15 16:11:39 -0300

    fix: estructura de componentes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Navbar } from "../index";
+import { Navbar, Footer } from "../index";
 import {
   Admin,
   Competencias,
@@ -8,23 +8,26 @@ import {
   Inicio,
   Noticias,
 } from "../../pages";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Box maxW="1200px" mx="auto" px={4}>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/federacion" element={<Federacion />} />
-          <Route path="/competencias" element={<Competencias />} />
-          <Route path="/noticias" element={<Noticias />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Box>
-    </div>
+    <>
+      <Flex direction="column" minHeight="100vh">
+        <Navbar />
+        <Box as="main" flex="1" width="100%" maxW="1200px" mx="auto" px={4}>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/federacion" element={<Federacion />} />
+            <Route path="/competencias" element={<Competencias />} />
+            <Route path="/noticias" element={<Noticias />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </Box>
+      </Flex>
+      <Footer />
+    </>
   );
 };
 

0c6b7f439c328a227099d4a2ac7a6cfa2ab8b904
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-10-16 11:07:23 -0300

    limpie codigo

---
+++ src/services/noticias.ts
@@ -0,0 +1,13 @@
+export const noticias = [
+  {
+    id: 1,
+    date: new Date(),
+    title:
+      "La Federación Sanluiseña de Deportes Acuáticos y Le Parc Club, los invitan al 𝐓𝐨𝐫𝐧𝐞𝐨‎ 𝐋𝐞‎ 𝐏𝐚𝐫𝐜‎ 𝐂𝐥𝐮𝐛‎ 𝟐𝟎𝟐𝟒 el próximo Sábado 5 de octubre",
+    content:
+      "<img src='/src/assets/imagen-prueba.jpg' /><br /><p>La Federación Sanluiseña de Deportes Acuáticos y Le Parc Club, los invitan al 𝐓𝐨𝐫𝐧𝐞𝐨‎ 𝐋𝐞‎ 𝐏𝐚𝐫𝐜‎ 𝐂𝐥𝐮𝐛‎ 𝟐𝟎𝟐𝟒 el próximo Sábado 5de octubre 🏆</p><br /><p>👉El mismo se llevará a cabo por la mañana en las instalaciones de @leparcclub en Villa Mercedes</p><br /><span>👉Invita a las categorías:</span><br /><ul><li>🔹Pre infantil 1 y 2.</li><li>🔹Infantil 1 y 2.</li><li>🔹Menores 1 y 2.</li><li>🔹Cadetes 1 y 2.</li><li>🔹Juveniles 1 y 2.</li></ul><br /><p>🏁 El programa de pruebas contempla las distancias de 25m para los cuatro estilos (mariposa, espalda, pecho y libre) y también 50m en dichos estilos!Además habrá relevo 4x25m libre y 4x50m libre mixto 🙌</p><p>‼️No te pierdas esta oportunidad, veni a disfrutar con tu equipo!</p><br /><span>✍️ 𝐈𝐧𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧𝐞𝐬‎ asdasanluis@yahoo.com.ar y vía WhatsApp:</span> <br/><span>📱𝐈𝐧𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧𝐞𝐬 2664-4218002</span><br /><span>Los esperamos 🫂</span>",
+    author: "Fany Zalazar",
+    categories: ["Invitación", "Torneo", "Inscripciones"],
+    url: "",
+  },
+];
